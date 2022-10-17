fabbb1461eaceff610a041a9ce26a8b6e3c52401
Matt Kerlogue <matt.kerlogue@me.com>
2022-10-17 19:44:01 +0100

    Update documentation

--- R/close_all.R
+++ R/close_all.R
@@ -1,13 +1,22 @@
-#' Close all open source files
+#' Close all open source files in RStudio
+#'
+#' This isn't necessary as is covered by an in-built function
+#' within the RStudio IDE and can be called by `cmd`+`shift`+`w` on Mac.
 #'
 #' @return invisibly the number of windows closed
 #' @export
+
+
 close_all <- function() {
 
   count <- 0
 
+  # get info about the source document in focus
+  # returns NULL when no open source documents
   check_source <- rstudioapi::getSourceEditorContext()
 
+  # documentClose() closes the document in focus
+  # loop closing and checking source until you get a NULL
   while (!is.null(check_source)) {
     rstudioapi::documentClose()
     count <- count + 1
