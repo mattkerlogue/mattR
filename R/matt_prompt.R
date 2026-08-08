ed494d0da2cf62c63b375c90a64cdb42da61e23f
Matt Kerlogue <matt.kerlogue@me.com>
2026-08-08 17:16:01 +0100

    Make custom prompt work in Positron

--- R/matt_prompt.R
+++ R/matt_prompt.R
@@ -2,42 +2,51 @@
 #'
 #' A function for use in an .Rprofile to update the R console prompt.
 #'
-#' Add the following to the .First() function in your .Rprofile file:
-#'   mattR::matt_prompt()
-#'   addTaskCallback(mattR::matt_prompt)
+#' Add the following code to the .First() function in your .Rprofile file:
+#'   ```mattR::matt_prompt()
+#'   addTaskCallback(mattR::matt_prompt)```
+#'
+#' Positron does not properly support `.First()`, in which case use `init_prompt()`
 #'
 #' @export
 matt_prompt <- function() {
-
   proj_path <- here::here()
   my_loc <- getwd()
 
   if (!is.null(proj_path)) {
-
     if (grepl(proj_path, my_loc)) {
-
       my_base <- basename(proj_path)
 
-      my_loc <- paste0(my_base, gsub(proj_path,  "", my_loc),
-                       collapse = .Platform$file.sep)
-
+      my_loc <- paste0(
+        my_base,
+        gsub(proj_path, "", my_loc),
+        collapse = .Platform$file.sep
+      )
     } else {
-
       home <- Sys.getenv("HOME")
 
       my_loc <- paste0("!! ", gsub(home, "~", my_loc))
     }
   }
 
-  git_branch <- suppressWarnings(system("git rev-parse --abbrev-ref HEAD",
-                                        ignore.stderr = TRUE, intern = TRUE))
+  git_branch <- suppressWarnings(system(
+    "git rev-parse --abbrev-ref HEAD",
+    ignore.stderr = TRUE,
+    intern = TRUE
+  ))
 
   if (length(git_branch) != 0) {
     git_msg <- paste0(" @", git_branch)
-    git_status <- suppressWarnings(system("git status -s",
-                                          ignore.stderr = TRUE, intern = TRUE))
-    git_ahead <- suppressWarnings(system("git status -sb",
-                                         ignore.stderr = TRUE, intern = TRUE))
+    git_status <- suppressWarnings(system(
+      "git status -s",
+      ignore.stderr = TRUE,
+      intern = TRUE
+    ))
+    git_ahead <- suppressWarnings(system(
+      "git status -sb",
+      ignore.stderr = TRUE,
+      intern = TRUE
+    ))
     git_ahead_chk <- grepl("ahead", git_ahead)
 
     if (length(git_status) != 0) {
@@ -45,19 +54,33 @@ matt_prompt <- function() {
     } else if (git_ahead_chk) {
       git_msg <- paste0(git_msg, " \u2b06")
     }
-
   } else {
     git_msg <- ""
   }
 
-  console_msg <- paste0("[",
-                        format(Sys.time(), "%H:%M"),
-                        " ", my_loc, "/",
-                        git_msg,
-                        "] > ")
+  console_msg <- paste0(
+    "[",
+    format(Sys.time(), "%H:%M"),
+    " ",
+    my_loc,
+    "/",
+    git_msg,
+    "] > "
+  )
 
   options(prompt = console_msg)
 
   invisible(TRUE)
+}
 
+#' @rdname matt_prompt
+#' @export
+init_prompt <- function() {
+  mattR::matt_prompt()
+
+  .new_prompt <- function(...) {
+    mattR::matt_prompt()
+  }
+
+  addTaskCallback(.new_prompt)
 }
