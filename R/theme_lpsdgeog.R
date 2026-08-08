950cc8f07e4de732e82303cf63e2e589ca03c7c9
Matt Kerlogue <matt.kerlogue@me.com>
2026-08-08 17:13:36 +0100

    Remove ggplot import

--- R/theme_lpsdgeog.R
+++ R/theme_lpsdgeog.R
@@ -5,65 +5,64 @@
 #' @export
 #'
 #' @param subtitle Whether the plot has a subtitle
-#'
-#' @import ggplot2
 
 theme_lpsdgeog <- function(subtitle = FALSE) {
-  t <- theme_void() +
-    theme(
-      text = element_text(
+  t <- ggplot2::theme_void() +
+    ggplot2::theme(
+      text = ggplot2::element_text(
         family = "Hack",
         colour = "grey40",
-        margin = margin(6, 6, 6, 6, "pt")
+        margin = ggplot2::margin(6, 6, 6, 6, "pt")
       ),
-      axis.title.x = element_text(
+      axis.title.x = ggplot2::element_text(
         hjust = 1,
         vjust = 0,
         size = 10,
         face = "bold"
       ),
-      axis.title.y = element_text(
+      axis.title.y = ggplot2::element_text(
         hjust = 1,
         vjust = 1,
         size = 10,
         face = "bold",
         angle = 90
       ),
-      axis.text = element_text(size = 8),
-      panel.grid = element_line(colour = "grey90"),
-      plot.margin = margin(6, 6, 6, 6, "pt")
+      axis.text = ggplot2::element_text(size = 8),
+      panel.grid = ggplot2::element_line(colour = "grey90"),
+      plot.margin = ggplot2::margin(6, 6, 6, 6, "pt")
     )
 
   if (subtitle) {
-    t <- t + theme(
-      plot.title = element_text(
-        face = "bold",
-        hjust = 1,
-        size = 12,
-        margin = margin(6, 6, 3, 6, "pt"),
-        vjust = 0,
-        lineheight = 1.1
+    t <- t +
+      ggplot2::theme(
+        plot.title = ggplot2::element_text(
+          face = "bold",
+          hjust = 1,
+          size = 12,
+          margin = ggplot2::margin(6, 6, 3, 6, "pt"),
+          vjust = 0,
+          lineheight = 1.1
         ),
-      plot.subtitle = element_text(
-        hjust = 1,
-        size = 8,
-        margin = margin(3, 6, 12, 6, "pt"),
-        vjust = 1
-    )
-    )
-  } else {
-    t <- t + theme(
-      plot.title = element_text(
-        face = "bold",
-        hjust = 1,
-        size = 12,
-        margin = margin(6, 6, 12, 6, "pt"),
-        vjust = 1,
-        lineheight = 1.1
+        plot.subtitle = ggplot2::element_text(
+          hjust = 1,
+          size = 8,
+          margin = ggplot2::margin(3, 6, 12, 6, "pt"),
+          vjust = 1
+        )
       )
-    )
+  } else {
+    t <- t +
+      ggplot2::theme(
+        plot.title = ggplot2::element_text(
+          face = "bold",
+          hjust = 1,
+          size = 12,
+          margin = ggplot2::margin(6, 6, 12, 6, "pt"),
+          vjust = 1,
+          lineheight = 1.1
+        )
+      )
   }
 
   return(t)
-
 }
